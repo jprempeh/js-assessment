c4f9d622113c1c45b89e66b4efe382a21dd36d10
Joe Prempeh <prempehjoe@gmail.com>
2015-09-08 20:56:58 -0700

    arrays

--- app/arrays.js
+++ app/arrays.js
@@ -48,27 +48,39 @@ exports.arraysAnswers = {
   },
 
   truncate : function(arr) {
-
+	  arr = arr.splice(0, arr.length - 1);
+	  return arr;
   },
 
   prepend : function(arr, item) {
-
+		arr.unshift(item);
+	  return arr;
   },
 
   curtail : function(arr) {
-
+		arr.shift();
+	  return arr;
   },
 
   concat : function(arr1, arr2) {
-
+		var newArr = arr1.concat(arr2);
+	  return newArr;
   },
 
   insert : function(arr, item, index) {
-
+		arr.splice(index, 0, item);
+	  return arr;
   },
 
   count : function(arr, item) {
-
+		var counter = 0,
+			i;
+	  for(i = 0; i < arr.length; i++) {
+		  if (arr[i] === item) {
+			  counter++;
+		  }
+	  }
+	  return counter;
   },
 
   duplicates : function(arr) {
